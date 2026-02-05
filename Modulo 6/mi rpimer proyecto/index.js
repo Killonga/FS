357c795f39a9b45ecb413ca9085d2dd83fb14487
Killonga <beatlemaniaweibel2@gmail.com>
2026-02-05 12:30:54 -0300

    se crea E1M6

---
+++ Modulo 6/mi rpimer proyecto/index.js
@@ -0,0 +1,28 @@
+console.log("hola Mundo!! desde Node.js!!");
+
+const fs = require('fs'); // 'fs' es el módulo de Node para interactuar con el File System
+
+console.log("--- Inicio del programa ---");
+
+// Operación Síncrona (Bloqueante)
+try {
+    const data = fs.readFileSync('archivo-inexistente.txt', 'utf8');
+    console.log("Contenido del archivo (síncrono):", data);
+} catch (err) {
+    console.error("Error al leer el archivo de forma síncrona.");
+}
+console.log("Después de la llamada síncrona.");
+
+
+// Operación Asíncrona (No Bloqueante)
+fs.readFile('archivo-inexistente.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error("Error al leer el archivo de forma asíncrona.");
+        return;
+    }
+    console.log("Contenido del archivo (asíncrono):", data);
+});
+console.log("Después de la llamada asíncrona.");
+
+
+console.log("--- Fin del programa ---");
